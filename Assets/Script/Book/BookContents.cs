8532df8c795782b55007926889ab616a3a581ecd
zhaqj12 <zhaqj12@naver.com>
2024-02-01 23:19:02 +0900

    빌드

--- Assets/Script/Book/BookContents.cs
+++ Assets/Script/Book/BookContents.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEditor.UI;
 using UnityEngine.UI;
 
 public class BookContents : MonoBehaviour
